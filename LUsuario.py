7744082b4b2191e96998c0bd35e3ec0a923c4f50
Carlos Andrade <carlosviansi@gmail.com>
2011-06-28 02:01:56 -0300

    Utilizando modulo de cookies de Tim

--- LUsuario.py
+++ LUsuario.py
@@ -2,7 +2,7 @@
 
 # Import the CGI, string, sys, and md5crypt modules
 
-import cgi, cgitb, string
+import cgi, cgitb, string, Cookie, os
 cgitb.enable()
 
 class LUsuario:
@@ -18,7 +18,47 @@ class LUsuario:
             return "passed"
         else:
             return "failed"
-     # Define function display_page.
+
+    def set_client_Cookie(self):
+        # Create a Cookie object.
+        a_cookie = Cookie.Cookie()
+
+        # Assign the cookie a value.
+        a_cookie["user"] = "Python"
+
+        # Required header that tells the browser how to render the HTML.
+        #print "Content-Type: text/html" informado na verificacao de senha
+
+        # Send the cookie back to the client.
+        print a_cookie, "\n\n"
+
+        # Print the cookie value.
+        print "<HTML><BODY>"
+        print a_cookie["user"].value, "user cookie set.\n"
+        print "<FORM METHOD = post ACTION = \"example_7.4.cgi\">\n"
+        print "<INPUT TYPE = \"hidden\" NAME = \
+        \"set\" VALUE =\"yes\">\n"
+        print "<INPUT TYPE = \"submit\" VALUE = \"Go\"></FORM>\n"
+        print "</BODY></HTML>\n"
+
+    # Define function to read a cookie.
+    def read_client_Cookie(self):
+        # Create a Cookie object.
+        a_cookie = Cookie.Cookie( os.environ.get("HTTP_COOKIE", "") )
+
+        # Assign the variable a cookie value.
+        cookie_val = a_cookie["user"].value
+
+        # Required header that tells the browser how to render the HTML.
+        print "Content-Type: text/html\n\n"
+
+        # Print the cookie value.
+        print "<HTML><BODY>"
+        print cookie_val, "user cookie read from client.\n"
+        print "</BODY></HTML>\n"
+
+
+    # Define function display_page.
     def display_page(self,result):
         print "Content-type:text/html\r\n\r\n"
         print "<HTML>\n"
@@ -40,5 +80,14 @@ def main():
         if (form["action"].value == "login"):
             result = lUsuario.test(form["username"].value, form["password"].value)
             lUsuario.display_page(result)
+
+            #if (form.has_key("set")):
+             #   if (form["set"].value == "yes"):
+                     #lUsuario.read_client_Cookie()
+                #else:
+            lUsuario.set_client_Cookie() 
+    
+
+
 #Call main function.
 main()
